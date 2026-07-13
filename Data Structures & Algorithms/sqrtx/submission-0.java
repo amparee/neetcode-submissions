6ae3f8262a7ec2ad4819d8079797064c5b0106cb
Pablo Servile <pablo.mcd@hotmail.com>
2026-07-12 22:10:58 -0300

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+        
+        int l = 0;
+        int r = x;
+        int ans = 0;
+
+        while (l <= r) {
+            int mid  = l + (r-l) / 2;
+            if (mid == 0) {
+                if (x >= 0) ans = 0;
+                l = mid + 1;
+                continue;
+            }
+            if (mid <= x / mid) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+}
